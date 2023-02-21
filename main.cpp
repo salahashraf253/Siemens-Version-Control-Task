5e6038ebb4a505632bdb02f01adc49225803ea5c
Abdullah <abdullahosama15@gmail.com>
2023-02-21 14:11:02 +0200

    feat: adding getAverage implementation

--- main.cpp
+++ main.cpp
@@ -10,8 +10,21 @@ int getSum(std::vector<int> arr)
     }
     return sum;
 }
+
+double getAverage(const std::vector<int>& vec) {
+
+    double avg = 0;
+    for(int i = 0; i < vec.size(); ++i) {
+        avg += vec[i];
+    }
+
+    return (vec.empty()) ? 0 : avg / vec.size();
+}
+
 int main()
 {
     std::vector<int>arr={1,2,3,4,5,6,7,8,9,10};
     std::cout<<getSum(arr)<<"\n";
+
+    std::cout << "Average: " << getAverage(arr) << "\n";
 }
